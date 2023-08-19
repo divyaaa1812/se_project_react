3f7b77d15173985a5934a911dc541ebc27c8dc2c
Divya B <divyaaa1812@gmail.com>
2023-08-18 18:47:56 -0700

    changes

--- wtwr/src/components/Header/Header.js
+++ wtwr/src/components/Header/Header.js
@@ -1,19 +1,25 @@
 import "./Header.css";
+import headerLogo from "../images/Logo.png";
+import avatar from "../images/avatar.png";
 
 function Header() {
   return (
     <div>
       <header className="header">
-        <div class="header__logo">
-          <div>Logo</div>
-          <div>Date</div>
-        </div>
-        <div class="header__avatar">
+        <div className="header__logo">
           <div>
-            <button type="text">Add New Clothes</button>
+            <img src={headerLogo} alt="WhatToWearLogo" />
           </div>
-          <div>Name</div>
-          <div>avatar</div>
+          <div className="header__date">Date</div>
+        </div>
+        <div className="header__avatar">
+          <div>
+            <button type="text">+ Add Clothes</button>
+          </div>
+          <div>Divya Bharathi</div>
+          <div className="header__avatar-image">
+            <img src={avatar} alt="profile Picture" />
+          </div>
         </div>
       </header>
     </div>
