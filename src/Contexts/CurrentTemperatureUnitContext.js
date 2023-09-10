8f2c5104279792d6b9ccfc3b3f9110d9d8d18cc4
Divya B <divyaaa1812@gmail.com>
2023-09-09 22:51:23 -0700

    changes

--- src/Contexts/CurrentTemperatureUnitContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-//create context obj with required values. This is to use across all components by wrapping it in html code
-const CurrentTemperatureUnitContext = React.createContext({
-  currentTemperatureUnit: "",
-  handleToggleSwitchChange: () => {},
-});
-
-export default CurrentTemperatureUnitContext;
